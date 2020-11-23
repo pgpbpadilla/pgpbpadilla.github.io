a8104d13e6e8e1ef398bea0dac64e837e1f4f8b1
Pablo Padilla <pgpb.padilla@gmail.com>
2020-11-23 17:40:01 +0100

    chg: dev: build site using docker image for jekyll

--- tasks.py
+++ tasks.py
@@ -2,6 +2,12 @@ from invoke import task
 
 
 @task
-def hello(ctx):
-    ctx.run('echo hello')
-    ctx.run('ls -la')
+def build(ctx, runner='docker'):
+    jekyll_build = 'jekyll build'
+    docker_build = 'docker run -v `pwd`:/srv/jekyll -p "4000:4000" jekyll/jekyll jekyll build'
+
+    build_cmd = docker_build
+    if runner is not 'docker':
+        build_cmd = jekyll_build
+
+    ctx.run(build_cmd)
