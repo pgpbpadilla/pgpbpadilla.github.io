82eedb03e229c7fc1cd0ecb6a661482d8bfa8590
Pablo Padilla <pgpb.padilla@gmail.com>
2021-01-09 21:16:37 +0100

    new: doc: Add help for Invoke tasks.

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ from invoke import task
 
 @task
 def build(ctx, docker=False):
+    """Use Jekyll to build the Blog"""
     jekyll_build = 'bundle exec jekyll build'
     docker_build = 'docker run -v `pwd`:/srv/jekyll -p "4000:4000" jekyll/jekyll jekyll build'
     build_cmd = docker_build
@@ -13,6 +14,7 @@ def build(ctx, docker=False):
 
 @task
 def serve(ctx, docker=False):
+    """Serves the Blog locally"""
     jekyll_serve = 'bundle exec jekyll serve'
     docker_serve = 'docker run -v `pwd`:/srv/jekyll -p "4000:4000" jekyll/jekyll jekyll serve'
     serve_cmd = docker_serve if docker else jekyll_serve
@@ -21,6 +23,7 @@ def serve(ctx, docker=False):
 
 @task
 def run(ctx, docker=False):
+    """Build+Run the Blog locally"""
     build(ctx, docker)
     serve(ctx, docker)
 
