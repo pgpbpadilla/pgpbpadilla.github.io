ad3ca67a97ea97c7b479dade0776ffe3b408fe6f
Pablo Padilla <pgpb.padilla@gmail.com>
2024-01-03 14:57:26 +0100

    fix: dev: should only publish _site

--- github.sh
+++ github.sh
@@ -15,14 +15,15 @@ REMOTE_REPO="https://${ACTOR}:${TOKEN}@github.com/${REPOSITORY}.git"
 echo "Deploying to ${REPOSITORY} on branch ${BRANCH}"
 echo "Deploying to ${REMOTE_REPO}"
 
+pushd _site
+
 git config --global init.defaultBranch main && \
   git init && \
   git config user.name "${ACTOR}" && \
   git config user.email "${ACTOR}@users.noreply.github.com" && \
   git add . && \
   git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
-  git push --force $REMOTE_REPO main:$BRANCH && \
-  (fuser -k .git || rm -rf .git) && \
-  cd ..
+  git push --force $REMOTE_REPO main:$BRANCH &&
 
-PROVIDER_EXIT_CODE=$?
+popd  
+
