6decf866bb54095184f86b746a1522260f886b20
Pablo Padilla <pgpb.padilla@gmail.com>
2024-01-04 21:25:17 +0100

    chg: dev: setting custom ids for headings generates nicer anchor ids

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,31 +3,18 @@
 
 ;; Adapted from:
 ;; https://librephoenix.com/2023-11-19-my-blog-setup-with-jekyll-org-mode-and-gitlab-pages
-((nil . (
-  (org-publish-project-alist .
-   (("html"
-          ;; Path to your org files.
-          :base-directory "~/blog/org/"
-          :base-extension "org"
-          ;; Path to your Jekyll project.
-          :publishing-directory "~/blog/org/jekyll/"
-          ;; Path is relative to :base-directory
-          :exclude "jekyll/assets/org/.*$" ;; exclude all Org assets
-          :recursive t
-          :publishing-function org-html-publish-to-html
-          :headline-levels 4
-          :html-extension "html"
-          :body-only t ;; Only export section between <body> </body>
-    )
-
-    ;; No need to duplicate assets, keep a single copy in org/jekyll/assets
-    ;; ("assets"
-    ;;       :base-directory "~/blog/org/"
-    ;;       :base-extension "svg\\|css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|php"
-    ;;       :publishing-directory "~/blog/jekyll/"
-    ;;       :recursive t
-    ;;       :publishing-function org-publish-attachment)
-
-    ("site" :components ("html"))))
-  )
-))
+((nil . ((org-publish-project-alist . (("site" :components ("html"))
+                                       ("html"
+                                        ;; Path to your org files.
+                                        :base-directory "~/blog/org/"
+                                        :base-extension "org"
+                                        ;; Path to your Jekyll project.
+                                        :publishing-directory "~/blog/org/jekyll/"
+                                        ;; Path is relative to :base-directory
+                                        :exclude "jekyll/assets/org/.*$" ;; exclude all Org assets
+                                        :recursive t
+                                        :publishing-function org-html-publish-to-html
+                                        :headline-levels 4
+                                        :html-extension "html"
+                                        ;; Only export section between <body> </body>
+                                        :body-only t))))))
